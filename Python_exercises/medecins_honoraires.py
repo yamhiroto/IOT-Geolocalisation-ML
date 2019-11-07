4cdac596adf738eb93c8032d6110dcd7e09780c0
savoga <savoure.gael@gmail.com>
2019-11-07 14:51:02 +0100

    code cleaning

--- Python_exercises/medecins_honoraires.py
+++ Python_exercises/medecins_honoraires.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Nov  5 12:49:45 2019
-
-@author: savoga
+07/11/2019
+Using data from medical websites, we want to analyze the link between doctor payments
+and their population by specialty
 """
 
 import pandas as pd
@@ -12,7 +12,7 @@ from difflib import SequenceMatcher
 def similar(a, b):
     return SequenceMatcher(None, a, b).ratio()
 
-def findCloserMatchVector(col_1, col_2, deg):
+def buildAndGetMatchingMap(col_1, col_2, deg):
     a={}
     for spe_1 in col_1:
         for spe_2 in col_2:
@@ -22,7 +22,7 @@ def findCloserMatchVector(col_1, col_2, deg):
             a[spe_1]=""
     return a
 
-# Following function looks more elegant however it takes much more time to compute
+# Following function looks more elegant (no double loop) however it takes much more time to compute
 #def findCloserWord(word_0):
 #    words = list(df_2['specialites'].unique())
 #    deg_simil_min = 0.6
@@ -38,11 +38,11 @@ def findCloserMatchVector(col_1, col_2, deg):
 #data = df_1['l_pre_spe'].apply(findCloserWord)
 
 
-# Dépassements/spécialité
+# Loading extra-fess per specialty
 df_1 = pd.read_csv("/home/savoga/Documents/Données_santé/n201808.csv", delimiter=";", encoding = "ISO-8859-1")
 df_1 = df_1[['l_pre_spe', 'dep_mon']]
 
-# Nombre de médecins/spécialité
+# Loading number of doctors per specialty
 df_2 = pd.read_excel("/home/savoga/Documents/Données_santé/medecins_specialites.xls")
 df_2 = df_2[8:]
 df_2 = df_2.rename(columns={"TABLEAU 1. EFFECTIFS DES MÉDECINS par spécialité, mode d'exercice, sexe et tranche d'âge": "specialites"})
@@ -50,7 +50,10 @@ df_2 = df_2.rename(columns = {"Unnamed: 1": "effectif"})
 df_2 = df_2[['specialites','effectif']]
 df_2['effectif'] = df_2['effectif'].astype(float)
 
-a=findCloserMatchVector(list(df_1['l_pre_spe'].unique()), df_2['specialites'], 0.7)
+# Build matching map for dataframe 1
+a=buildAndGetMatchingMap(list(df_1['l_pre_spe'].unique()), df_2['specialites'], 0.7)
+# Some specialty has no corresponding value from our matching function;
+# We fill the empty cells with human intuition
 a["02-Anesthésiologie - Réanimation chirurgicale"]="Anesthésie-réanimation"
 a["03-Pathologie cardio-vasculaire"]="Cardiologie et maladies vasculaires"
 a["18-Stomatologie"]="Chirurgie maxillo-faciale et stomatologie"
@@ -60,6 +63,7 @@ a["42-Endocrinologie"]="Endocrinologie et métabolisme"
 a["74-Oncologie radiothérapique"]="Oncologie option médicale"
 a["77-Obstétrique"]="Gynécologie-obstétrique"
 
+# Get matched specialties
 df_1['spe_2']=df_1['l_pre_spe'].apply(lambda x: a[x])
 df_1 = df_1[df_1.spe_2 != ''].reset_index(drop=True)
 df_1['dep_mon'] = df_1['dep_mon'].str.replace(',','.')
@@ -68,7 +72,14 @@ df_1['dep_mon']=df_1['dep_mon'].astype(float)
 df_1 = df_1.groupby(['spe_2'])['dep_mon'].agg('sum')
 df_1=df_1.reset_index()
 
+# Perform the join
 df_3 = df_1.join(df_2.set_index('specialites'), on='spe_2')
+
+# Histograms
 df_3.plot(x='spe_2', y='dep_mon', kind='bar', legend=False, figsize=(15, 10))
 df_3.plot(x='spe_2', y='effectif', kind='bar', legend=False, figsize=(15, 10))
+
+# Correlation coefficient
 print("la correlation entre les effectifs et le dépassement monétaire est {}".format(df_3['effectif'].corr(df_3['dep_mon'])))
+
+# => not significant link
