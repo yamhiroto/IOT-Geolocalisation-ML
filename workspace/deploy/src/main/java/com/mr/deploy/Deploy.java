8f8a5defd98241d33454ab18d3a0f7cab62968ab
savoga <savoure.gael@gmail.com>
2019-12-25 20:02:27 +0100

    clean and deploy through maven

--- workspace/deploy/src/main/java/com/mr/deploy/Deploy.java
+++ workspace/deploy/src/main/java/com/mr/deploy/Deploy.java
@@ -1,3 +1,4 @@
+package com.mr.deploy;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -11,35 +12,38 @@ import java.util.concurrent.TimeUnit;
 /***
  * @author gsavoure
  *
- * Question 1. STEP 8
+ * Question 2. STEP 7
  *
  */
 
-public class CLEAN_MR {
+public class Deploy {
 
     public static final String FOLDER_PATH = "/tmp/";
     public static final String FOLDER_NAME = "savoga";
     public static final String SSH_COMMAND = "ssh";
-    public static final String FOLDER_DELETION_COMMAND = "rm -rf";
+    public static final String FOLDER_CREATION_COMMAND = "mkdir -p";
     public static final String LS_COMMAND = "ls";
+    public static final String FOLDER_RESOURCES = "src/main/resources";
     public static final String HOSTNAME_COMMAND = "hostname";
     public static final String USER_PREFIX = "gsavoure@";
-    public static final String HOME_FOLDER = "/cal/homes/gsavoure/";
+    public static final String COPY_COMMAND = "scp";
+    public static final String SLAVE_FILENAME = "SLAVE_MR.jar";
+    public static final String HOME_FOLDER = "/home/savoga/";
 
     public static void main(String[] args) throws IOException {
-        String fileName = "machines_test.txt";
+        String fileName = FOLDER_RESOURCES + "/machines.txt";
         List<String> wordList = getWordList(fileName);
 
         for (int i = 0; i < wordList.size(); i++) {
             boolean isConnected = false;
             String machineName = wordList.get(i);
-            //machineName = USER_PREFIX + machineName;
+            machineName = USER_PREFIX + machineName;
             try {
                 // *** Connection to the machine ***
                 System.out.println("Trying to connect to " + machineName);
-                ProcessBuilder processBuilder = new ProcessBuilder(SSH_COMMAND, USER_PREFIX + machineName, HOSTNAME_COMMAND);
+                ProcessBuilder processBuilder = new ProcessBuilder(SSH_COMMAND, machineName, HOSTNAME_COMMAND);
                 MyThread myThread = startThread(processBuilder);
-                String valueThread = myThread.getQueue().poll(2, TimeUnit.SECONDS);
+                String valueThread = myThread.getQueue().poll(3, TimeUnit.SECONDS);
                 if (valueThread == null) {
                     System.out.println(machineName + " is KO");
                 } else {
@@ -47,14 +51,21 @@ public class CLEAN_MR {
                     isConnected = true;
                 }
 
-                // *** Folder deletion ***
-                if (isConnected) {
-                    while (folderExist(machineName)) {
-                        ProcessBuilder processBuilder2 = new ProcessBuilder(SSH_COMMAND, USER_PREFIX + machineName, FOLDER_DELETION_COMMAND, FOLDER_PATH + FOLDER_NAME);
-                        System.out.println("Folder deleted on machine " + machineName);
+                // *** Folder creation ***
+                if(isConnected) {
+                    while(!folderExist(machineName)) {
+                        ProcessBuilder processBuilder2 = new ProcessBuilder(SSH_COMMAND, machineName, FOLDER_CREATION_COMMAND, FOLDER_PATH + FOLDER_NAME);
+                        System.out.println("Folder created on machine " + machineName);
                         MyThread myThreadDir = startThread(processBuilder2);
                     }
+
+                    // *** Copy of slave.jar ***
+                    ProcessBuilder processBuilder3 = new ProcessBuilder(COPY_COMMAND,  HOME_FOLDER + "/Documents/" + SLAVE_FILENAME, machineName + ":" + FOLDER_PATH + FOLDER_NAME + "/");
+                    System.out.println("Copying jar file on " + machineName);
+                    MyThread myThreadCopy = startThread(processBuilder3);
+                    Thread.sleep(4000);
                 }
+
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -62,7 +73,7 @@ public class CLEAN_MR {
     }
 
     public static boolean folderExist(String machineName) {
-        ProcessBuilder processBuilder = new ProcessBuilder(SSH_COMMAND, USER_PREFIX + machineName, LS_COMMAND, FOLDER_PATH);
+        ProcessBuilder processBuilder = new ProcessBuilder(SSH_COMMAND, machineName, LS_COMMAND, FOLDER_PATH);
         MyThread myThread = startThread(processBuilder);
         try {
             Thread.sleep(5000); // let some time so that the queue gets filled
@@ -70,11 +81,10 @@ public class CLEAN_MR {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        if (myThread.getQueue().contains(FOLDER_NAME)) {
+        if(myThread.getQueue().contains(FOLDER_NAME)) {
             System.out.println("Folder " + FOLDER_NAME + " found in " + FOLDER_PATH);
             return true;
-        }
-        ;
+        };
         System.out.println("No folder named " + FOLDER_NAME);
         return false;
     }
@@ -97,4 +107,3 @@ public class CLEAN_MR {
     }
 
 }
-
--- workspace/deploy/src/main/java/com/mr/deploy/MyThread.java
+++ workspace/deploy/src/main/java/com/mr/deploy/MyThread.java
@@ -1,3 +1,5 @@
+package com.mr.deploy;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
